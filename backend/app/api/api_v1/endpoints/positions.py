b654dc54cc31c8435fca1a1009eec7276f923f19
B0arrr <aw055966@student.ath.edu.pl>
2022-05-01 19:56:01 +0200

    Complete CRUD for Positions

--- backend/app/api/api_v1/endpoints/positions.py
+++ backend/app/api/api_v1/endpoints/positions.py
@@ -1,9 +1,10 @@
 from typing import Any, List
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Body, HTTPException
+from fastapi.encoders import jsonable_encoder
 from sqlalchemy.orm import Session
 
-from app import schemas, models, crud
+from app import schemas, crud
 from app.api import deps
 
 router = APIRouter()
@@ -21,9 +22,9 @@ def create_position(
         position_in: schemas.PositionCreate
 ) -> Any:
     """
-    Create new item.
+    Create new position
     """
-    position = crud.position.create_one(db=db, obj_in=position_in)
+    position = crud.position.create(db=db, obj_in=position_in)
     return position
 
 
@@ -33,7 +34,10 @@ def get_position_by_id(
         db: Session = Depends(deps.get_db),
         id: int
 ) -> Any:
-    positions = crud.position.get_by_id(db=db, id=id)
+    """
+    Get position by id
+    """
+    positions = crud.position.get(db=db, id=id)
     return positions
 
 
@@ -43,6 +47,9 @@ def get_position_id_by_name(
         db: Session = Depends(deps.get_db),
         name: str
 ) -> Any:
+    """
+    Get position id by name
+    """
     return crud.position.get_id(db=db, name=name)
 
 
@@ -52,6 +59,9 @@ def get_position_name_by_id(
         db: Session = Depends(deps.get_db),
         id: int
 ) -> Any:
+    """
+    Get position name by id
+    """
     return crud.position.get_name_by_id(db=db, id=id)
 
 
@@ -60,4 +70,45 @@ def get_all_positions(
         *,
         db: Session = Depends(deps.get_db)
 ) -> Any:
+    """
+    Get all positions
+    """
     return crud.position.get_all(db=db)
+
+
+@router.put("/update_position_by_id/{id}/name/{name}", response_model=schemas.Position)
+def update_position(
+        *,
+        db: Session = Depends(deps.get_db),
+        id: int,
+        name: str
+) -> Any:
+    """
+    Update position by id
+    """
+    position = crud.position.get(db=db, id=id)
+    if not position:
+        raise HTTPException(
+            status_code=404,
+            detail="Position with this id don't exist"
+        )
+    position_to_update = jsonable_encoder(position)
+    position_updated = schemas.PositionUpdate(**position_to_update)
+    position_updated.name = name
+    return crud.position.update(db=db, db_obj=position, obj_in=position_updated)
+
+
+@router.delete("/delete_position/{id}", response_model=schemas.Position)
+def delete_position(
+    *,
+    db: Session = Depends(deps.get_db),
+    id: int
+) -> Any:
+    """
+    Delete a position
+    """
+    position = crud.position.get(db=db, id=id)
+    if not position:
+        raise HTTPException(status_code=404, detail="Position not found")
+    position = crud.position.remove(db=db, id=id)
+    return position
--- backend/app/crud/crud_position.py
+++ backend/app/crud/crud_position.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Dict, Union, Any
 
 from fastapi.encoders import jsonable_encoder
 from sqlalchemy.orm import Session
@@ -9,23 +9,6 @@ from app.schemas.position import PositionCreate, PositionUpdate
 
 
 class CRUDPosition(CRUDBase[Position, PositionCreate, PositionUpdate]):
-    def create_one(
-            self, db: Session, *, obj_in: PositionCreate
-    ) -> Position:
-        obj_in_data = jsonable_encoder(obj_in)
-        db_obj = self.model(**obj_in_data)
-        db.add(db_obj)
-        db.commit()
-        db.refresh(db_obj)
-        return db_obj
-
-    def get_by_id(
-            self, db: Session, *, id: int
-    ) -> Position:
-        return db.query(self.model) \
-            .filter(Position.id == id) \
-            .first()
-
     def get_id(
             self, db: Session, *, name: str
     ) -> int:
