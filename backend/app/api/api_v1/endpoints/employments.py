75eedcc88028ebf7fe2e8894ee071478b233a020
B0arrr <aw055966@student.ath.edu.pl>
2022-05-01 19:56:15 +0200

    Complete CRUD for Employments

--- backend/app/api/api_v1/endpoints/employments.py
+++ backend/app/api/api_v1/endpoints/employments.py
@@ -1,6 +1,7 @@
 from typing import Any, List
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.encoders import jsonable_encoder
 from sqlalchemy.orm import Session
 
 from app import schemas, crud
@@ -16,8 +17,11 @@ def create_employment(
         db: Session = Depends(get_db),
         employment_in: schemas.EmploymentCreate
 ) -> Any:
-    position = crud.employment.create_one(db=db, obj_in=employment_in)
-    return position
+    """
+    Create employment
+    """
+    employment = crud.employment.create(db=db, obj_in=employment_in)
+    return employment
 
 
 @router.get("/get_employment_by_id/{id}", response_model=schemas.Employment)
@@ -26,8 +30,11 @@ def get_employment_by_id(
         db: Session = Depends(deps.get_db),
         id: int
 ) -> Any:
-    positions = crud.employment.get_by_id(db=db, id=id)
-    return positions
+    """
+    Get employment by id
+    """
+    employments = crud.employment.get(db=db, id=id)
+    return employments
 
 
 @router.get("/get_employment_id/{name}")
@@ -36,6 +43,9 @@ def get_employment_id_by_name(
         db: Session = Depends(deps.get_db),
         name: str
 ) -> Any:
+    """
+    Get employment id by employment
+    """
     return crud.employment.get_id(db=db, name=name)
 
 
@@ -45,6 +55,9 @@ def get_employment_name_by_id(
         db: Session = Depends(deps.get_db),
         id: int
 ) -> Any:
+    """
+    Get employment name by id
+    """
     return crud.employment.get_name_by_id(db=db, id=id)
 
 
@@ -53,4 +66,89 @@ def get_all_employments(
         *,
         db: Session = Depends(deps.get_db)
 ) -> Any:
+    """
+    Get all employments
+    """
     return crud.employment.get_all(db=db)
+
+
+@router.put("/update_employment_by_id/{id}/employment/{name}", response_model=schemas.Employment)
+def update_employment_name(
+        *,
+        db: Session = Depends(deps.get_db),
+        id: int,
+        name: str
+) -> Any:
+    """
+    Update employment name
+    """
+    employment = crud.employment.get(db=db, id=id)
+    if not employment:
+        raise HTTPException(
+            status_code=404,
+            detail="Employment with this id don't exist"
+        )
+    employment_to_update = jsonable_encoder(employment)
+    employment_updated = schemas.EmploymentUpdate(**employment_to_update)
+    employment_updated.employment = name
+    return crud.employment.update(db=db, db_obj=employment, obj_in=employment_updated)
+
+
+@router.put("/update_employment_by_id/{id}/hours_per_week/{hours}", response_model=schemas.Employment)
+def update_employment_name(
+        *,
+        db: Session = Depends(deps.get_db),
+        id: int,
+        hours: int
+) -> Any:
+    """
+    Update employment name
+    """
+    employment = crud.employment.get(db=db, id=id)
+    if not employment:
+        raise HTTPException(
+            status_code=404,
+            detail="Employment with this id don't exist"
+        )
+    employment_to_update = jsonable_encoder(employment)
+    employment_updated = schemas.EmploymentUpdate(**employment_to_update)
+    employment_updated.hours_per_week = hours
+    return crud.employment.update(db=db, db_obj=employment, obj_in=employment_updated)
+
+
+@router.put("/update_employment_by_id/{id}/hours_per_day/{hours}", response_model=schemas.Employment)
+def update_employment_name(
+        *,
+        db: Session = Depends(deps.get_db),
+        id: int,
+        hours: int
+) -> Any:
+    """
+    Update employment name
+    """
+    employment = crud.employment.get(db=db, id=id)
+    if not employment:
+        raise HTTPException(
+            status_code=404,
+            detail="Employment with this id don't exist"
+        )
+    employment_to_update = jsonable_encoder(employment)
+    employment_updated = schemas.EmploymentUpdate(**employment_to_update)
+    employment_updated.hours_per_day = hours
+    return crud.employment.update(db=db, db_obj=employment, obj_in=employment_updated)
+
+
+@router.delete("/delete_employment/{id}", response_model=schemas.Employment)
+def delete_employment(
+        *,
+        db: Session = Depends(deps.get_db),
+        id: int
+) -> Any:
+    """
+    Delete employment
+    """
+    employment = crud.employment.get(db=db, id=id)
+    if not employment:
+        raise HTTPException(status_code=404, detail="Employment not found")
+    employment = crud.employment.remove(db=db, id=id)
+    return employment
--- backend/app/crud/crud_employment.py
+++ backend/app/crud/crud_employment.py
@@ -9,23 +9,6 @@ from app.schemas import EmploymentCreate, EmploymentUpdate
 
 
 class CRUDEmployment(CRUDBase[Employment, EmploymentCreate, EmploymentUpdate]):
-    def create_one(
-            self, db: Session, *, obj_in: EmploymentCreate
-    ) -> Employment:
-        obj_in_data = jsonable_encoder(obj_in)
-        db_obj = self.model(**obj_in_data)
-        db.add(db_obj)
-        db.commit()
-        db.refresh(db_obj)
-        return db_obj
-
-    def get_by_id(
-            self, db: Session, *, id: int
-    ) -> Employment:
-        return db.query(Employment) \
-            .filter(Employment.id == id) \
-            .first()
-
     def get_id(
             self, db: Session, *, name: str
     ) -> int:
