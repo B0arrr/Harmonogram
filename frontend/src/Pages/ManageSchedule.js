856fba8b06a784af7f73b287f8d1880a861ed5c7
Kowal031 <73485596+Kowal031@users.noreply.github.com>
2022-06-09 14:23:03 +0200

    Added new pages file

---
+++ frontend/src/Pages/ManageSchedule.js
@@ -0,0 +1,31 @@
+
+import React from "react";
+import HeaderManageSchedule from "../Components/Header/HeaderManageSchedule";
+import SchedulePicker from "../Components/SchedulePicker";
+import EmployeeScheduleGenerator from "../Components/EmployeeScheduleGenerator";
+import Schedule from "../Components/Schedule";
+import'../Components/Style/ManageSchedule.css'
+
+function ManageSchedule() {
+    return (
+        <div>
+            <HeaderManageSchedule/>
+            <div className='containerForManageSchedule'>
+                <div className='employeeScheduleGenerator'>
+                    <EmployeeScheduleGenerator/>
+                </div>
+                <div className='employeeSchedulePicker'>
+                    <SchedulePicker/>
+                </div>
+            </div>
+            <div className='employeeSchedule'>
+                <Schedule/>
+            </div>
+
+
+        </div>
+    );
+}
+
+
+export default ManageSchedule;
---
+++ frontend/src/Pages/RegistrationPage.js
@@ -0,0 +1,17 @@
+
+import React from "react";
+import Registration from "../Components/Registration";
+import HeaderRegistration from "../Components/Header/HeaderRegistration";
+
+
+function RegistrationPage(){
+
+    return(
+        <div>
+            <HeaderRegistration/>
+            <Registration/>
+       </div>
+    )
+}
+
+export default RegistrationPage
